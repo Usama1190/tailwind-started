7ec90ad07278b71449d6db162c4832932a12aff2
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-08-18 13:09:58 -0700

    Keep learning on Aug 19, 24

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,77 @@
 import Image from "next/image";
+import CarWheel from "@/components/Wheel";
+import CarHeadlight from "@/components/Headlight";
+import CarSeats from "@/components/Seats";
 
 export default function Home() {
   return (
-    <div className="flex justify-center bg-sky-900">
-      <div className="m-4 order-last">
-        <Image src={'/Untitled design (1).jpg'} 
-        alt="Usama" width={100} height={100}/>
-      </div>
-
-      <div className="mx-4 self-center text-center">
-        <h1 className="text-6xl font-bold text-zinc-200">Usama Israr</h1>
-        <h2 className="text-3xl font-semibold text-zinc-200">Teaching Web3 and Metaverse Classes</h2>
-        <button className="my-4 px-4 py-2 border-2 text-zinc-200 border-zinc-200 rounded-lg">Learn More</button>
-      </div>
+    <div>
+      <div className="bg-blue-500 h-24 w-52 hover:scale-150"></div>
+      <CarWheel />
+      <CarWheel />
+      <CarWheel />
+      <CarWheel />
+      <CarHeadlight />
+      <CarHeadlight />
+      <CarSeats />
+      <CarSeats />
+      <CarSeats />
+      <CarSeats />
+      <CarSeats />
     </div>
+
+
+
+
+
+    // <div>
+    //   <div className="bg-blue-500 h-24 w-52 hover:h-32 hover:w-60"></div>
+    //   <CarWheel />
+    //   <CarWheel />
+    //   <CarWheel />
+    //   <CarWheel />
+    //   <div className="bg-yellow-500 h-5 w-5"></div>
+    //   <div className="bg-yellow-500 h-5 w-5"></div>
+    // </div>
+
+
+
+
+
+    // <div>
+    //   <div className="bg-blue-500 h-24 w-52"></div>
+    //   <div className="bg-black h-12 w-12"></div>
+    //   <div className="bg-black h-12 w-12"></div>
+    //   <div className="bg-black h-12 w-12"></div>
+    //   <div className="bg-black h-12 w-12"></div>
+    //   <div className="bg-yellow-500 h-5 w-5"></div>
+    //   <div className="bg-yellow-500 h-5 w-5"></div>
+    // </div>
+
+
+
+
+    // <div>
+    //   <p className="text-xs">This is the one with 12px font size.</p>
+    //   <p className="text-lg">This is the one with 18px font size.</p>
+    //   <p className="text-2xl">This is the one with 24px font size.</p>
+    // </div>
+
+
+
+
+
+    // <div className="flex justify-center bg-sky-900">
+    //   <div className="m-4 order-last">
+    //     <Image src={'/Untitled design (1).jpg'} 
+    //     alt="Usama" width={100} height={100}/>
+    //   </div>
+
+    //   <div className="mx-4 self-center text-center">
+    //     <h1 className="text-6xl font-bold text-zinc-200">Usama Israr</h1>
+    //     <h2 className="text-3xl font-semibold text-zinc-200">Teaching Web3 and Metaverse Classes</h2>
+    //     <button className="my-4 px-4 py-2 border-2 text-zinc-200 border-zinc-200 rounded-lg">Learn More</button>
+    //   </div>
+    // </div>
   );
 }
---
+++ components/Seats.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function CarSeats() {
+    return <div className="bg-amber-600 h-8 w-8"></div>
+    
+}
+
+export default CarSeats;
